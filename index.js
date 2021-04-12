938371053457b196f5efb015eede76867cc00c68
Chau Tran <cctran96@gmail.com>
2021-04-12 14:29:01 -0700

    commit tree a108627e72784b1595fb99522dc3542c674533c7

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-var cats = ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendCat(name) {
     cats.push(name)
-    return cats
 }
 
 function destructivelyPrependCat(name) {
     cats.unshift(name)
-    return cats
 }
 
 function destructivelyRemoveLastCat() {
     cats.pop()
-    return cats
 }
 
 function destructivelyRemoveFirstCat() {
     cats.shift()
-    return cats
 }
 
 function appendCat(name) {
-    return [...cats, name]
+    let arr = [...cats, name]
+    return arr
 }
 
 function prependCat(name) {
-    return [name, ...cats]
+    let arr = [name, ...cats] 
+    return arr
 }
 
 function removeLastCat() {
-    return cats.slice(0, -1)
+    return cats.slice(0, cats.length - 1)
 }
 
 function removeFirstCat() {
-    return cats.slice(1)
+    return cats.slice(1, cats.length)
 }
